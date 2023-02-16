aa0ad8ed7058998745e8244cda4d54e370df041d
Julien Loizelet <julienloizelet@okaeli.com>
2023-02-16 13:52:47 +0900

    feat(log): Add some log during bouncing process

--- src/AbstractBouncer.php
+++ src/AbstractBouncer.php
@@ -84,6 +84,9 @@ abstract class AbstractBouncer
         $forcedTestIp = $this->getConfig('forced_test_ip');
         $ip = $forcedTestIp ?: $this->getRemoteIp();
         $ip = $this->handleForwardedFor($ip, $this->configs);
+        $this->logger->info('Bouncing current IP', [
+            'ip' => $ip,
+        ]);
         $remediation = $this->getRemediationForIp($ip);
         $this->handleRemediation($remediation, $ip);
     }
@@ -356,9 +359,15 @@ abstract class AbstractBouncer
     {
         switch ($remediation) {
             case Constants::REMEDIATION_CAPTCHA:
+                $this->logger->debug('Will display a captcha wall', [
+                    'ip' => $ip,
+                ]);
                 $this->handleCaptchaRemediation($ip);
                 break;
             case Constants::REMEDIATION_BAN:
+                $this->logger->debug('Will display a ban wall', [
+                    'ip' => $ip,
+                ]);
                 $this->handleBanRemediation();
                 break;
             case Constants::REMEDIATION_BYPASS:
@@ -454,11 +463,19 @@ abstract class AbstractBouncer
             $maxRemediationLevel,
             $orderedRemediations
         );
+        $finalRemediation = $remediation;
         if ($currentIndex < $maxIndex) {
-            return $orderedRemediations[$maxIndex];
+            $finalRemediation = $orderedRemediations[$maxIndex];
+            $this->logger->debug('Original remediation has been capped', [
+                'origin' => $remediation,
+                'final' => $finalRemediation
+            ]);
         }
+        $this->logger->info('Final remediation', [
+            'remediation' => $finalRemediation,
+        ]);
 
-        return $remediation;
+        return $finalRemediation;
     }
 
     /**
@@ -614,6 +631,9 @@ abstract class AbstractBouncer
         if (null === $cachedCaptchaVariables['has_to_be_resolved']) {
             // Set up the first captcha remediation.
             $mustResolve = true;
+            $this->logger->debug('First captcha resolution', [
+                'ip' => $ip,
+            ]);
             $this->initCaptchaResolution($ip);
         }
 
