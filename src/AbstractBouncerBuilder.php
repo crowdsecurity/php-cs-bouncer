40d274c1f1034812275f62cb9180c3ada97901aa
Julien Loizelet <julienloizelet@okaeli.com>
2023-09-01 16:02:38 +0900

    feat(*): Refactor using a single AbstractBouncerBuilder class

---
+++ src/AbstractBouncerBuilder.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CrowdSecBouncer;
+
+use CrowdSec\CapiClient\Client\CapiHandler\Curl as CapiCurl;
+use CrowdSec\CapiClient\Client\CapiHandler\FileGetContents as CapiFileGetContents;
+use CrowdSec\CapiClient\Storage\StorageInterface;
+use CrowdSec\CapiClient\Watcher as WatcherClient;
+use CrowdSec\Common\Client\RequestHandler\Curl;
+use CrowdSec\Common\Client\RequestHandler\FileGetContents;
+use CrowdSec\LapiClient\Bouncer as BouncerClient;
+use CrowdSec\RemediationEngine\AbstractRemediation;
+use CrowdSec\Common\Client\AbstractClient;
+use CrowdSec\RemediationEngine\LapiRemediation;
+use Psr\Log\LoggerInterface;
+
+/**
+ * The class to create a LAPI or CAPI bouncer
+ *
+ * @author    CrowdSec team
+ *
+ * @see      https://crowdsec.net CrowdSec Official Website
+ *
+ * @copyright Copyright (c) 2021+ CrowdSec
+ * @license   MIT License
+ */
+abstract class AbstractBouncerBuilder extends AbstractBouncer
+{
+    public function __construct(array $configs, StorageInterface $storage = null, LoggerInterface $logger = null)
+    {
+        $remediationEngine = $this->buildRemediationEngine($configs, $storage, $logger);
+        parent::__construct($configs, $remediationEngine, $logger);
+    }
+
+    private function buildRemediationEngine(
+        array $configs,
+        StorageInterface $storage = null,
+        LoggerInterface $logger = null
+    ): AbstractRemediation {
+        $cache = $this->handleCache($configs, $logger);
+
+        $client = $this->buildClient($configs, $storage, $logger);
+
+        if ($client instanceof WatcherClient) {
+            return new CapiRemediation($configs, $client, $cache, $logger);
+        }
+
+        return new LapiRemediation($configs, $client, $cache, $logger);
+    }
+
+    private function buildClient(
+        array $configs,
+        StorageInterface $storage = null,
+        LoggerInterface $logger = null
+    ): AbstractClient {
+        $useCurl = !empty($configs['use_curl']);
+
+        if ($storage) {
+            $requestHandler = $useCurl ? new CapiCurl($configs) : new CapiFileGetContents($configs);
+
+            return new WatcherClient($configs, $storage, $requestHandler, $logger);
+        }
+
+        $requestHandler = $useCurl ? new Curl($configs) : new FileGetContents($configs);
+
+        return new BouncerClient($configs, $requestHandler, $logger);
+    }
+}
